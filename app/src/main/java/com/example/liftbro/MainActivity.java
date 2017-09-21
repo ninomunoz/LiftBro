7ce4bdb03d714c18f331aa25c49631712e3c3f99
Nino Munoz <mfmunoz@verisk.com>
2017-09-20 20:52:45 -0600

    feat: add up navigation to toolbar

--- app/src/main/java/com/example/liftbro/MainActivity.java
+++ app/src/main/java/com/example/liftbro/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.liftbro;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +28,35 @@ public class MainActivity extends AppCompatActivity {
             WorkoutFragment workoutFragment = WorkoutFragment.newInstance();
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.fragment_container, workoutFragment).commit();
+
+            getSupportFragmentManager().addOnBackStackChangedListener(
+                    new FragmentManager.OnBackStackChangedListener() {
+                        @Override
+                        public void onBackStackChanged() {
+                            if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+                                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                                getSupportActionBar().setHomeButtonEnabled(true);
+                            }
+                            else {
+                                getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+                            }
+                        }
+                    }
+            );
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                FragmentManager fm = getSupportFragmentManager();
+                if (fm.getBackStackEntryCount() > 0) {
+                    fm.popBackStack();
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
