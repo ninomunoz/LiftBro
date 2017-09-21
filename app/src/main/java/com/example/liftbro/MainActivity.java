7f6ed518531be306deb72d640580ce058f92ca06
Nino Munoz <mfmunoz@verisk.com>
2017-09-20 20:19:05 -0600

    refactor: use toolbar instead of action bar

--- app/src/main/java/com/example/liftbro/MainActivity.java
+++ app/src/main/java/com/example/liftbro/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.liftbro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         if (findViewById(R.id.fragment_container) != null) {
 
