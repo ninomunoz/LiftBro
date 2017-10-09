f41ddcde8d21150a0b0d43c04bf96904d1f1804b
Nino Munoz <mfmunoz@verisk.com>
2017-10-09 16:53:40 -0600

    chore: remove unused methods from DummyExercise class

--- app/src/main/java/com/example/liftbro/model/DummyExercise.java
+++ app/src/main/java/com/example/liftbro/model/DummyExercise.java
@@ -5,7 +5,6 @@ package com.example.liftbro.model;
  */
 
 public class DummyExercise {
-    int mId;
     String mName;
     int mSets = 0;
     int mReps = 0;
@@ -19,44 +18,8 @@ public class DummyExercise {
         mWeight = weight;
     }
 
-    public DummyExercise(int id, String name, int sets, int reps, double weight) {
-        mId = id;
-        mName = name;
-        mSets = sets;
-        mReps = reps;
-        mWeight = weight;
-    }
-
     public DummyExercise(String name, int time) {
         mName = name;
         mTime = time;
-    }
-
-    public DummyExercise(int id, String name, int time) {
-        mId = id;
-        mName = name;
-        mTime = time;
-    }
-
-    public void setId(int id) { mId = id; }
-    public void setName(String name) { mName = name; }
-    public void setSets(int sets) { mSets = sets; }
-    public void setReps(int reps) { mReps = reps; }
-    public void setWeight(double weight) { mWeight = weight; }
-    public void setTime(int time) { mTime = time; }
-
-    public int getId() { return mId; }
-    public String getName() {
-        return mName;
-    }
-    public int getSets() {
-        return mSets;
-    }
-    public int getReps() {
-        return mReps;
-    }
-    public double getWeight() { return mWeight; }
-    public int getTime() {
-        return mTime;
     }
 }
