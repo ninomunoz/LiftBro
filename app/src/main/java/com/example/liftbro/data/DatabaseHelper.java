c62f90b9d232ef1962fb2981e6f3661dd42a4591
Nino Munoz <mfmunoz@verisk.com>
2017-10-14 11:07:58 -0600

    refactor: import static tables from DatabaseContract to DatabaseHelper

--- app/src/main/java/com/example/liftbro/data/DatabaseHelper.java
+++ app/src/main/java/com/example/liftbro/data/DatabaseHelper.java
@@ -4,6 +4,11 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import static com.example.liftbro.data.DatabaseContract.MuscleGroupEntry;
+import static com.example.liftbro.data.DatabaseContract.WorkoutEntry;
+import static com.example.liftbro.data.DatabaseContract.ExerciseEntry;
+import static com.example.liftbro.data.DatabaseContract.WorkoutExerciseEntry;
+
 /**
  * Created by i57198 on 10/9/17.
  */
@@ -14,42 +19,42 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
 
     private static final String SQL_CREATE_TABLE_MUSCLE_GROUP =
-            "CREATE TABLE " + DatabaseContract.MuscleGroupEntry.TABLE_NAME + " (" +
-            DatabaseContract.MuscleGroupEntry._ID + " INTEGER PRIMARY KEY, " +
-            DatabaseContract.MuscleGroupEntry.COLUMN_NAME + " TEXT)";
+            "CREATE TABLE " + MuscleGroupEntry.TABLE_NAME + " (" +
+            MuscleGroupEntry._ID + " INTEGER PRIMARY KEY, " +
+            MuscleGroupEntry.COLUMN_NAME + " TEXT)";
 
     private static final String SQL_CREATE_TABLE_WORKOUT =
-            "CREATE TABLE "  + DatabaseContract.WorkoutEntry.TABLE_NAME + " (" +
-                    DatabaseContract.WorkoutEntry._ID + " INTEGER PRIMARY KEY, " +
-                    DatabaseContract.WorkoutEntry.COLUMN_NAME + " TEXT)";
+            "CREATE TABLE "  + WorkoutEntry.TABLE_NAME + " (" +
+                    WorkoutEntry._ID + " INTEGER PRIMARY KEY, " +
+                    WorkoutEntry.COLUMN_NAME + " TEXT)";
 
     private static final String SQL_CREATE_TABLE_EXERCISE =
-            "CREATE TABLE "  + DatabaseContract.ExerciseEntry.TABLE_NAME + " (" +
-                    DatabaseContract.ExerciseEntry._ID + " INTEGER PRIMARY KEY, " +
-                    DatabaseContract.ExerciseEntry.COLUMN_MUSCLEGROUP_ID + " INTEGER, " +
-                    DatabaseContract.ExerciseEntry.COLUMN_NAME + " TEXT)";
+            "CREATE TABLE "  + ExerciseEntry.TABLE_NAME + " (" +
+                    ExerciseEntry._ID + " INTEGER PRIMARY KEY, " +
+                    ExerciseEntry.COLUMN_MUSCLEGROUP_ID + " INTEGER, " +
+                    ExerciseEntry.COLUMN_NAME + " TEXT)";
 
     private static final String SQL_CREATE_TABLE_WORKOUT_EXERCISE =
-            "CREATE TABLE "  + DatabaseContract.WorkoutExerciseEntry.TABLE_NAME + " (" +
-                    DatabaseContract.WorkoutExerciseEntry._ID + " INTEGER PRIMARY KEY, " +
-                    DatabaseContract.WorkoutExerciseEntry.COLUMN_WORKOUT_ID + " INTEGER, " +
-                    DatabaseContract.WorkoutExerciseEntry.COLUMN_EXERCISE_ID + " INTEGER, " +
-                    DatabaseContract.WorkoutExerciseEntry.COLUMN_SETS + " INTEGER, " +
-                    DatabaseContract.WorkoutExerciseEntry.COLUMN_REPS + " INTEGER, " +
-                    DatabaseContract.WorkoutExerciseEntry.COLUMN_WEIGHT + " REAL, " +
-                    DatabaseContract.WorkoutExerciseEntry.COLUMN_TIME + " INTEGER)";
+            "CREATE TABLE "  + WorkoutExerciseEntry.TABLE_NAME + " (" +
+                    WorkoutExerciseEntry._ID + " INTEGER PRIMARY KEY, " +
+                    WorkoutExerciseEntry.COLUMN_WORKOUT_ID + " INTEGER, " +
+                    WorkoutExerciseEntry.COLUMN_EXERCISE_ID + " INTEGER, " +
+                    WorkoutExerciseEntry.COLUMN_SETS + " INTEGER, " +
+                    WorkoutExerciseEntry.COLUMN_REPS + " INTEGER, " +
+                    WorkoutExerciseEntry.COLUMN_WEIGHT + " REAL, " +
+                    WorkoutExerciseEntry.COLUMN_TIME + " INTEGER)";
 
     private static final String SQL_DROP_TABLE_MUSCLE_GROUP =
-            "DROP TABLE IF EXISTS " + DatabaseContract.MuscleGroupEntry.TABLE_NAME;
+            "DROP TABLE IF EXISTS " + MuscleGroupEntry.TABLE_NAME;
 
     private static final String SQL_DROP_TABLE_WORKOUT =
-            "DROP TABLE IF EXISTS " + DatabaseContract.WorkoutEntry.TABLE_NAME;
+            "DROP TABLE IF EXISTS " + WorkoutEntry.TABLE_NAME;
 
     private static final String SQL_DROP_TABLE_EXERCISE =
-            "DROP TABLE IF EXISTS " + DatabaseContract.ExerciseEntry.TABLE_NAME;
+            "DROP TABLE IF EXISTS " + ExerciseEntry.TABLE_NAME;
 
     private static final String SQL_DROP_TABLE_WORKOUT_EXERCISE =
-            "DROP TABLE IF EXISTS" + DatabaseContract.WorkoutExerciseEntry.TABLE_NAME;
+            "DROP TABLE IF EXISTS" + WorkoutExerciseEntry.TABLE_NAME;
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
